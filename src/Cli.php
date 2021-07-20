46a13ec8fb5cfda88b1cdcdecb416341c480dfe2
Roman <roman@MacBook-Pro-Roman-2.local>
2021-07-20 21:57:05 +0700

    add lint and git actions

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function testFunc(){
+function testFunc()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
